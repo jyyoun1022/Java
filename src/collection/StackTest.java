590577b454614c883690429798a5a80ed3b26031
younjaeyeal <jyyoun1021@gmail.com>
2022-04-30 17:42:48 +0900

    HashMap 사용

---
+++ src/collection/StackTest.java
@@ -0,0 +1,16 @@
+package collection;
+
+public class StackTest {
+    public static void main(String[] args) {
+        MyStack stack = new MyStack();
+
+        stack.push("A");
+        stack.push("B");
+        stack.push("C");
+
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+
+    }
+}
